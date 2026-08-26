e367fb11cd5c86df6edd9135c90cda4d1f0efc6d
Bruce Watanabe <ywatanabe007@yahoo.com>
2026-08-25 17:39:59 -0700

    Fix MySQL SSL config - use empty ssl object

--- lib/mysql-sync.ts
+++ lib/mysql-sync.ts
@@ -25,7 +25,7 @@ function getPool(): mysql.Pool {
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-      ssl: { rejectUnauthorized: false },
+      ssl: {},
       waitForConnections: true,
       connectionLimit: 5,
       queueLimit: 0,
